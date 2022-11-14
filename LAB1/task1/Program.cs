a1bf594e68952aecd6dbfffd3ee33ec410212396
Arsenii Dobrovolskyi <cleverboyars@gmail.com>
2022-11-14 21:52:41 +0200

    Random number generator

---
+++ LAB1/task1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Generating random numbers
+using System;
+
+public class Generator
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("15 numbers were generated for you:");
+        Console.WriteLine(" ");
+
+        Random rand = new Random();
+
+        for (int i = 0; i < 15; i++)
+        {
+            Console.WriteLine("{0, 3}", rand.Next(-1000, 1000));
+        }
+
+        Console.WriteLine(" ");
+        Console.WriteLine("That's it!");
+    }
+}
---
+++ LAB1/task2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Generating random numbers using RNGCryptoServiceProvider
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var randNumGen = new RNGCryptoServiceProvider())
+        {
+            var randNum = new byte[5]; //creating a byte array (5 bytes is the length of our random number)
+
+            Console.WriteLine("Numbers generated using RNGCryptoServiceProvider:");
+            Console.WriteLine(" ");
+
+            for (int i = 0; i < 15; i++)
+            {
+                randNumGen.GetBytes(randNum); //byte array filling with random data
+                string num = Convert.ToBase64String(randNum);
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine(" ");
+            Console.WriteLine("That's it!");
+        }
+    }
+}
